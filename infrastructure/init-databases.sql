c0776ad1cf80d83518f728814bcc1cc78ce7edf8
bb-code1 <barkatbashir10@gmail.com>
2026-07-22 18:01:27 +0530

    feat: initialize project infrastructure with Eureka discovery server, PostgreSQL, Redis, and Kafka docker stack

---
+++ infrastructure/init-databases.sql
@@ -0,0 +1,17 @@
+-- SQL Initialization script for XBS Labs Microservices Databases
+-- Automatically executed on PostgreSQL container startup
+
+-- 1. Auth & Identity Microservice Database
+CREATE DATABASE naqashly_auth_db;
+CREATE USER auth_user WITH ENCRYPTED PASSWORD 'auth_password_123';
+GRANT ALL PRIVILEGES ON DATABASE naqashly_auth_db TO auth_user;
+
+-- 2. Finance & Ledger Microservice Database
+CREATE DATABASE naqashly_finance_db;
+CREATE USER finance_user WITH ENCRYPTED PASSWORD 'finance_password_123';
+GRANT ALL PRIVILEGES ON DATABASE naqashly_finance_db TO finance_user;
+
+-- 3. Productivity & Habits Microservice Database
+CREATE DATABASE naqashly_productivity_db;
+CREATE USER productivity_user WITH ENCRYPTED PASSWORD 'productivity_password_123';
+GRANT ALL PRIVILEGES ON DATABASE naqashly_productivity_db TO productivity_user;
