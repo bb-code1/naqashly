96907dd2b93fd739a3d3b8d943dc2213d966744d
bb-code1 <barkatbashir10@gmail.com>
2026-08-10 12:41:27 +0530

    feat: add docker-compose configuration and database initialization scripts for microservices stack

---
+++ backend/docker-init-scripts/init-db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE naqashly_auth_db;
+CREATE DATABASE naqashly_finance_db;
+CREATE DATABASE naqashly_productivity_db;
+CREATE DATABASE naqashly_routine_db;
+CREATE DATABASE naqashly_journal_db;
