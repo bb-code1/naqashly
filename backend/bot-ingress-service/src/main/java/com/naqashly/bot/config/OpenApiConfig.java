4875adc203a7497f220602748c9a0c2ccc512f54
bb-code1 <barkatbashir10@gmail.com>
2026-08-11 08:54:48 +0530

    feat: initialize backend service structures with Spring Boot, Eureka discovery, and OpenAPI documentation configurations

---
+++ backend/bot-ingress-service/src/main/java/com/naqashly/bot/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.naqashly.bot.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <h1>OpenAPI Documentation & Security Scheme Configuration</h1>
+ */
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "Naqashly Bot Ingress Service API", version = "v1.0.0", description = "Telegram Webhook & Message Ingress Handler Service API Documentation"),
+    security = @SecurityRequirement(name = "X-User-Id")
+)
+@SecurityScheme(
+    name = "X-User-Id",
+    type = SecuritySchemeType.APIKEY,
+    in = SecuritySchemeIn.HEADER,
+    paramName = "X-User-Id",
+    description = "Gateway-forwarded authenticated User ID header"
+)
+public class OpenApiConfig {
+}
